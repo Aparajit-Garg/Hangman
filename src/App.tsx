90892919255e10bf9c4679da09335817ba1355cd
Aparajit <aparajit.garg06@gmail.com>
2023-12-12 16:39:43 +0530

    initial setup using vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState("Test")
+  
+  return (
+    <>
+      <h1> Hi </h1>
+    </>
+  )
+}
+
+export default App
