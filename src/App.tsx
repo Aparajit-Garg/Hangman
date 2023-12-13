4258b535cd27d563e1d9bcf7f51adc594a6d26c2
Aparajit <aparajit.garg06@gmail.com>
2023-12-14 01:22:10 +0530

    keyboard, onscreen input added, keys styled for input

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect, useCallback } from "react"
 import words from './wordList.json'
 import classes from './App.module.css'
 import HangmanWord from "./components/HangmanWord";
@@ -14,6 +14,26 @@ function App() {
 
 	const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
 	
+	const addGuessedLetter = useCallback((key: string) => {
+		if (guessedLetters.includes(key)) return
+		
+		setGuessedLetters(prev => [...prev, key])
+	}, [guessedLetters])
+
+	useEffect(() => {
+		const handler = (e: KeyboardEvent) => {
+			const key = e.key;
+			if (!key.match(/^[a-z]$/)) return;
+			
+			e.preventDefault()
+			addGuessedLetter(key)
+		}
+
+		document.addEventListener('keypress', handler)
+
+		return () => document.removeEventListener('keypress', handler)
+	}, [guessedLetters])
+
 	console.log('random word to guess: ', wordToGuess)
 	return (
 		<div className={classes.outer_container}>
@@ -22,7 +42,7 @@ function App() {
 			</div>
 			<HangmanDrawing numberOfGuesses={incorrectLetters.length} />
 			<HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
-			<HangmanKeyboard />
+			<HangmanKeyboard activeLetter={guessedLetters.filter(letter => wordToGuess.includes(letter))} inActiveLetters={incorrectLetters} addGuessedLetters={addGuessedLetter} />
 		</div>
 	)
 }
--- src/components/HangmanKeyboard.tsx
+++ src/components/HangmanKeyboard.tsx
@@ -1,12 +1,20 @@
 import classes from '../styles/HangmanKeyboard.module.css'
 import keys from '../keyboardKeys'
 
-const HangmanKeyboard = () => {
+type HangmanKeyboardProps = {
+	activeLetter: string[],
+	inActiveLetters: string[],
+	addGuessedLetters: (letter: string) => void
+}
+
+const HangmanKeyboard = ({ activeLetter, inActiveLetters, addGuessedLetters } : HangmanKeyboardProps) => {
 	return (
 		<div className={classes.container}>
 			{keys.map((key) => {
+				const isActive = activeLetter.includes(key)
+				const inActive = inActiveLetters.includes(key)
 				return (
-					<button className={`${classes.keys}`} key={key}>
+					<button onClick={() => addGuessedLetters(key)} className={`${classes.keys} ${isActive ? classes.active : ''} ${inActive ? classes.inactive : ''}`} key={key}>
 						{key}
 					</button>
 				)
