c7fd10219eeede9753b496886f73ab19c3eea7a0
Aparajit <aparajit.garg06@gmail.com>
2023-12-13 15:23:00 +0530

    hangman drawing done

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,15 +1,45 @@
 import classes from '../styles/HangmanDrawing.module.css'
 
+const HEAD = () => (
+    <div className={classes.head}></div>
+)
+
+const BODY = () => (
+    <div className={classes.body}></div>
+)
+
+const LeftHand = () => (
+    <div className={classes.left_hand}></div>
+)
+
+const RightHand = () => (
+    <div className={classes.right_hand}></div>
+)
+
+const LeftLeg = () => (
+    <div className={classes.left_leg}></div>
+)
+
+const RightLeg = () => (
+    <div className={classes.right_leg}></div>
+)
+
 const HangmanDrawing = () => {
   return (
     <div className={classes.container}>
         <div className={classes.top_horizontal}>
             <div className={classes.hanging_line}>
-        </div>
+                <HEAD />
+                <BODY />
+                <LeftHand />
+                <RightHand />
+                <LeftLeg />
+                <RightLeg />
+
+            </div>
         </div>
         <div className={classes.vertical_line}></div>
         <div className={classes.base}></div>
-       
     </div>
   )
 }
